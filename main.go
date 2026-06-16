3c48aed2c3da058500a7755fcca6ecd2c898228e
alex <alex.kiselev.dev@gmail.com>
2026-06-16 17:04:42 +0100

    added service and json file, timestamp

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"time"
+	//"github.com/lib/pq"
+)
+
 func main() {
 
+	listExpenses([]Expense{
+		{ID: 1, Amount: 50.0, Category: "Food", Note: "Lunch", Date: time.Now().Format(time.RFC3339)},
+		{ID: 2, Amount: 20.0, Category: "Transport", Note: "Bus fare", Date: time.Now().Format(time.RFC3339)},
+		{ID: 3, Amount: 100.0, Category: "Shopping", Note: "Clothes", Date: time.Now().Format(time.RFC3339)},
+	})
 }
---
+++ service.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func taskTimestamp() time.Time {
+	return time.Now().UTC().Truncate(time.Second)
+}
+
+func nextExpenseID(expenses []Expense) int {
+	maxID := 0
+	for _, expense := range expenses {
+		if expense.ID > maxID {
+			maxID = expense.ID
+		}
+	}
+	return maxID + 1
+}
+
+func addExpense(expenses []Expense, amount float64, category string) []Expense {
+	timestamp := taskTimestamp()
+	newExpense := Expense{
+		Amount:   amount,
+		Category: category,
+		Date:     timestamp.Format("2006-01-02 15:04:05"),
+		Note:     "",
+		ID:       nextExpenseID(expenses),
+	}
+	return append(expenses, newExpense)
+}
+
+func listExpenses(expenses []Expense) {
+	printExpenseHeader()
+	for _, expense := range expenses {
+		printExpenseRow(expense)
+	}
+}
+
+func printExpenseHeader() {
+	fmt.Printf("%-4s %-12s %-16s %-16s %-16s\n", "ID", "AMOUNT", "NOTE", "CATEGORY", "DATE")
+}
+
+func printExpenseRow(expense Expense) {
+	fmt.Printf("%-4d %-12s %-16.2f %-16s %-16s\n", expense.ID, expense.Category, expense.Amount, expense.Note, expense.Date)
+}
